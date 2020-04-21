4e537a9c0d30b7f8dea53061145e26e23b1806c3
[kelsodumez] <[17232@burnside.school.nz]>
2020-04-21 15:07:38 +1200

    created links between .py .css and .html files, began to plan what site would look like

--- gaem.py
+++ gaem.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return "login"
     
+@app.route("/games")
+def games():
+    return "games"
 if __name__ == "__main__":
     app.run(debug=True)
