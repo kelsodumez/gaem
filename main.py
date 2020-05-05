b134da9c6270837dacf0210b9ad33ff44a029200
kelsodumez <17232@burnside.school.nz>
2020-05-05 16:20:07 +1200

    set up some sqlalchemy things, also began setting up html templates

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, Blueprint
 from markupsafe import escape
+from . import db
 
-app = Flask(__name__)#defines the app as a flask application
+main = Blueprint('main', __name__)
 
-app.secret_key = b'\x82\xc4\x0f\x8e\x02\x91q\xab|6-cy\xfct\x18'#the secret key used for creating user specific sessions
+@main.route('/')#home page
+def index():
+    return 'Index'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@app.route("/")#this creates the homepage that users will default to when loading the site, unless they specify otherwise
-def home():
-    return render_template("home.html")
-
-@app.route("/login")#login page
-def login():
-    return "login"
-    
-@app.route("/games")#this is the page that will be used to list all games in the database
-def games():
-    return "games"
-
-if __name__ == "__main__":#the code that runs the website, debug=true makes it so that saving will auto update the site
-    app.run(debug=True)
+@main.route('/profile')#profile page
+def profile():
+    return 'Profile'
