e0870206cbd9802851487b932242095f38403c4e
Kelso du Mez <17232@burnside.school.nz>
2020-05-19 14:40:22 +1200

    tutorial was bad starting again :(

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, session, redirect, url_for, request, Blueprint
-from markupsafe import escape
-from . import db
+import os
+from flask_sqlalchemy import SQLAlchemy
 
-main = Blueprint('main', __name__)
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@main.route('/')#home page
+db = SQLAlchemy(app)
+
+@app.route('/')#home page
 def index():
     return render_template('index.html')
 
-@main.route('/profile')#profile page
+@app.route('/profile')#profile page
 def profile():
     return render_template('profile.html')
