c44c5ce9374daa20c3467c35180144ae98413ed5
kelsodumez <17232@burnside.school.nz>
2020-05-22 08:44:02 +1200

    finally got git working after three lessons of work

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "gaem.db")) # links to the database
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-@app.route('/')#home page
-def index():
+class game(db.Model):
+    name = db.Column(db.Text(80), unique=True, nullable=False, primary_key=False)
+    dateadded = db.Column(db.Date, unique=False, nullable=False, primary_key=False)
+    #useradded = db.Column(db.Text(80), unique=False, nullable=False, primary_key=False)
+    description = db.Column(db.Text(80), unique=True, nullable=False, primary_key=False)
+    datepublished = db.Column(db.Date, unique=False, nullable=False, primary_key=False)
+    #publisher = db.Column(db.integer(80), unique=False, nullable=False, primary_key=False)
+    #developer = db.Column(db.integer(80), unique=False, nullable=False, primary_key=False)
+
+@app.route('/')# home page
+def home():
     return render_template('home.html')
-'''
-@app.route('/profile')#profile page
-def profile():
-    return render_template('profile.html')
-'''
+
+@app.route('/index')# index for games
+def index():
+    game = game.query.all()
+    return render_template('home.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
