bd6b7bd6f4298dc4967516d5a9b60f95209e96be
kelsodumez <techniclogical@gmail.com>
2020-06-09 09:27:27 +1200

    added code comments, worked on a variable dependant game page and fixed navbar

--- main.py
+++ main.py
@@ -12,42 +12,38 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
+'''
+Below is all the classes needed to define data from the database
+'''
 class Comment(db.Model):
     __tablename__ = 'comments'
     ID = db.Column(db.Integer, primary_key=True)
     gameid = db.Column(db.ForeignKey('games.ID'))
     userid = db.Column(db.ForeignKey('userinfo.ID'))
     comment = db.Column(db.Text)
-
     game = db.relationship('Game', primaryjoin='Comment.gameid == Game.ID', backref='comments')
     userinfo = db.relationship('Userinfo', primaryjoin='Comment.userid == Userinfo.ID', backref='comments')
 
 class Developer(db.Model):
     __tablename__ = 'developers'
-
     ID = db.Column(db.Integer, primary_key=True)
     developername = db.Column(db.Text)
 
 class Gamegenre(db.Model):
     __tablename__ = 'gamegenre'
-
     ID = db.Column(db.Integer, primary_key=True)
     gameid = db.Column(db.ForeignKey('games.ID'))
     genreid = db.Column(db.Integer)
-
     game = db.relationship('Game', primaryjoin='Gamegenre.gameid == Game.ID', backref='gamegenres')
 
 class Genre(Gamegenre):
     __tablename__ = 'genres'
-
     ID = db.Column(db.ForeignKey('gamegenre.ID'), primary_key=True)
     genrename = db.Column(db.Text)
     description = db.Column(db.Text)
 
 class Game(db.Model):
     __tablename__ = 'games'
-
     ID = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, unique=True)
     dateadded = db.Column(db.Date)
@@ -56,20 +52,17 @@ class Game(db.Model):
     datepublished = db.Column(db.Date)
     publisher = db.Column(db.ForeignKey('publishers.publishername'))
     developer = db.Column(db.ForeignKey('developers.developername'))
-
     developer1 = db.relationship('Developer', primaryjoin='Game.developer == Developer.developername', backref='games')
     publisher1 = db.relationship('Publisher', primaryjoin='Game.publisher == Publisher.publishername', backref='games')
     userinfo = db.relationship('Userinfo', primaryjoin='Game.useradded == Userinfo.username', backref='games')
 
 class Publisher(db.Model):
     __tablename__ = 'publishers'
-
     ID = db.Column(db.Integer, primary_key=True)
     publishername = db.Column(db.Text)
 
 class Rating(db.Model):
     __tablename__ = 'ratings'
-
     ID = db.Column(db.Integer, primary_key=True)
     gameid = db.Column(db.ForeignKey('games.ID'))
     userid = db.Column(db.ForeignKey('userinfo.ID'))
@@ -80,12 +73,13 @@ class Rating(db.Model):
 
 class Userinfo(db.Model):
     __tablename__ = 'userinfo'
-
     ID = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text)
     password = db.Column(db.Text)
     isadmin = db.Column(db.Boolean)
-
+'''
+Below is all the routes for each webpage
+'''
 @app.route('/')  # home page
 def home():
     return render_template('home.html')
@@ -95,6 +89,13 @@ def index():
     game=None
     game=Game.query.all()
     return render_template('index.html', game=game)
+
+@app.route('/game/<int:id>')
+def videogame(id):
+    print(id)
+    game=None
+    game=Game.query.all()
+    return render_template('game.html', game=game)
 
 if __name__ == "__main__":
     app.run(debug=True)
